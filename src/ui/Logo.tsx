42f51b260d72ad7bbe376943e861e67fccf30c43
Khyle Duterte <khyle.dev@gmail.com>
2024-11-08 18:02:15 +0400

    Update Logo.tsx

--- src/ui/Logo.tsx
+++ src/ui/Logo.tsx
@@ -1,7 +1,7 @@
 const Logo = () => {
   return (
     <div className="md:ml-10 text-4xl md:text-2xl">
-      <span className="font-bold">WORD</span>
+      <span className="font-bold">TEXT</span>
       <span className="italic text-[#4CCD99]">FALLS</span>
     </div>
   );
